e5d563db2bb74d4fbf8ec2c0abdec82f80e6b55e
aouos <aouos@qq.com>
2020-12-01 17:45:53 +0800

    init eat

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/eat.png" alt="">
+    <p>What to eat</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 8em;
+  text-shadow: 10px 10px 20px #888888;
+}
+</style>
